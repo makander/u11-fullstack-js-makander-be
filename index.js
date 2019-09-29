687af2f95dc6403676b60f539a018d0a811899a0
Nils Makander <makander@gmail.com>
2019-09-29 17:53:54 +0200

    change vars for port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const chalk = require('chalk');
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(chalk.red.bold('Welcome to CoffeePot BE'));
 });
